463a25dc0102662195f98ca31386305dda0a0ca6
abdlerahman mohamed <boda8099@gmail.com>
2024-03-29 22:56:44 +0200

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+* binary_tree_delete - Deletes an entire binary tree
+*
+* @tree: the tree
+*
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+	return;
+	}
+
+	free(tree->left);
+	free(tree->right);
+	free(tree);
+}

533744d69c3f9f856add977bf5f482dc2aa080ca
abdlerahman mohamed <boda8099@gmail.com>
2024-03-30 01:30:52 +0200

    h++

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
 /**
 * binary_tree_height - measures the height of a binary tree
@@ -10,22 +11,34 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_depth, right_depth;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 	{
 	return (0);
 	}
-
-	left_depth = binary_tree_height(tree->left);
-	right_depth = binary_tree_height(tree->right);
-
-	if (left_depth > right_depth)
+	else
 	{
-	return (left_depth + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (my_max(left_height, right_height) + 1);
+	}
+}
+/**
+* my_max - maxes greater heighter...etc}
+* @a: is a greater than b?
+* @b: is b greater than a?
+*
+* Return: any the winner
+*/
+size_t my_max(size_t a, size_t b)
+{
+	if (a >= b)
+	{
+	return (a);
 	}
 	else
 	{
-	return (right_depth + 1);
+	return (b);
 	}
 }

8b771cc684da977add28b144e034a06f1f0db5bc
abdlerahman mohamed <boda8099@gmail.com>
2024-03-29 02:06:30 +0200

    binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,14 +15,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (parent == NULL || new_node == NULL)
+	if (new_node == NULL)
 	{
 	return (NULL);
 	}
 
 	new_node->left = NULL;
 	new_node->right = NULL;
-	new_node->value = value;
+	new_node->n = value;
+    new_node->parent = parent;
 
 	return (new_node);
 }

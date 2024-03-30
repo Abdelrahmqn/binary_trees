0df944c5abd83de01f498cf0d325c2d523918234
abdlerahman mohamed <boda8099@gmail.com>
2024-03-30 05:00:11 +0200

    t

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,20 +8,10 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes;
-
-
 	if (tree == NULL)
 	{
 	return (0);
 	}
 
-	if (tree->left != NULL || tree->right != NULL)
-	{
-	nodes++;
-	}
-	nodes += binary_tree_node(tree->left);
-	nodes += binary_tree_node(tree->right);
-
-	return (nodes);
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
